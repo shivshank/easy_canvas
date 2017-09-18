8c815974a6b22a008d4a5e2e2526ceee3d6ead6c
shivshank <shivshank@users.noreply.github.com>
2017-09-17 21:27:56 -0400

    Make build script more reliable

--- quick_window/build.rs
+++ quick_window/build.rs
@@ -1,5 +1,13 @@
 fn main() {
     #[cfg(windows)] {
-        println!("cargo:rustc-link-search=native=glfw");
+        use std::env;
+        use std::path::PathBuf;
+        
+        let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+        let mut path = PathBuf::new();
+        path.push(dir);
+        path.push("glfw");
+        println!("cargo:warning={:?}", path);
+        println!("cargo:rustc-link-search=native={}", path.to_string_lossy());
     }
 }
