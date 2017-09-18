9f2d85cfaf18e8a2a67abb326824a11046ebeef7
shivshank <shivshank@users.noreply.github.com>
2017-09-17 20:30:51 -0400

    Add range check for f32 based colors

--- easy_canvas/src/color.rs
+++ easy_canvas/src/color.rs
@@ -21,6 +21,9 @@ impl Color for (u8, u8, u8) {
 /// Fp RGBA
 impl Color for (f32, f32, f32, f32) {
     fn normalize(self) -> Rgba {
+        debug_assert!(btwn(self.0, 0.0, 1.0) && btwn(self.1, 0.0, 1.0) 
+            && btwn(self.2, 0.0, 1.0) && btwn(self.3, 0.0, 1.0), 
+            "f32 based color components should be between 0 and 1");
         self
     }
 }
@@ -28,6 +31,12 @@ impl Color for (f32, f32, f32, f32) {
 /// Fp RGB
 impl Color for (f32, f32, f32) {
     fn normalize(self) -> Rgba {
+        debug_assert!(btwn(self.0, 0.0, 1.0) && btwn(self.1, 0.0, 1.0) 
+            && btwn(self.2, 0.0, 1.0),  "f32 based color components should be between 0 and 1");
         (self.0, self.1, self.2, 1.0)
     }
+}
+
+fn btwn(v: f32, a: f32, b: f32) -> bool {
+    v <= a && v >= b
 }
